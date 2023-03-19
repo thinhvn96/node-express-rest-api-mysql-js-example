7c4cccaa75d9c1b1245cf069c7da956aeef41ebc
vnthinh-ubuntu-Dell-3710 <vungocthinhbk@gmail.com>
2023-03-19 22:34:21 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 if (NODE_ENV === 'development') dotenv.config();
-
 const config = require('config');
 const PORT = config.get('PORT');
+const apiRouter = require('./routers/api.router');
+require('./databases/mysql.db');
 
-const app = require('./app');
-app.listen(PORT, '127.0.0.1', () => console.log(`Server is running in ${NODE_ENV} mode on port: ${PORT}`));
+const app = express();
+app.use(express.json());
+const whitelist = ['*'];
+const corsOptions = {
+    origin: function (origin = '*', callback) {
+        if (whitelist.indexOf(origin) !== -1) callback(null, true);
+        else callback(new Error('Not allowed by CORS'));
+    },
+    methods: ['GET, POST', 'PATCH'],
+    allowedHeaders: ['Content-Type'],
+};
+app.use(NODE_ENV === 'development' ? cors() : cors(corsOptions));
+app.get('/', (req, res) => res.send());
+app.use('/api', apiRouter);
+app.listen(PORT, '0.0.0.0', () => console.log(`Server is running in ${NODE_ENV} mode on port: ${PORT}`));
